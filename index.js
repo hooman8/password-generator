23e4c7b91e42f7efc1387708f41cd37ce5d66c19
Hooman Pegahmehr <hpegahmehr@gmail.com>
2023-08-02 15:26:35 -0400

    include special characters

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const crypto = require("crypto");
 
 function generatePassword(length = 16) {
-  return crypto.randomBytes(length).toString("base64").slice(0, length);
+  const specialChars = "!@#$%^&*()_+=-";
+  const password = crypto
+    .randomBytes(length)
+    .toString("base64")
+    .slice(0, length - 1);
+  const specialChar =
+    specialChars[Math.floor(Math.random() * specialChars.length)];
+  return password + specialChar;
 }
 
 console.log(generatePassword());
